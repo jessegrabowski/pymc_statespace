Fixed errors in simulations when covariance matrices are not full rank. Updated ARMA notebook to new API.

pymc_statespace/core/statespace.py
@@ -213,7 +213,7 @@ class PyMCStateSpace:
             if posterior_samples > 1.0 or posterior_samples < 0.0:
                 raise ValueError('If posterior_samples is a float, it should be between 0 and 1, representing the '
                                  'fraction of total posterior samples to re-sample.')
-            posterior_samples = np.floor(posterior_samples * posterior_size)
+            posterior_samples = int(np.floor(posterior_samples * posterior_size))
 
         elif posterior_samples is None:
             posterior_samples = posterior_size
@@ -324,7 +324,7 @@ class PyMCStateSpace:
             if posterior_samples > 1.0 or posterior_samples < 0.0:
                 raise ValueError('If posterior_samples is a float, it should be between 0 and 1, representing the '
                                  'fraction of total posterior samples to re-sample.')
-            posterior_samples = np.floor(posterior_samples * posterior_size)
+            posterior_samples = int(np.floor(posterior_samples * posterior_size))
 
         elif posterior_samples is None:
             posterior_samples = posterior_size

pymc_statespace/models/SARIMAX.py
@@ -1,6 +1,9 @@
 from pymc_statespace.core.statespace import PyMCStateSpace
+from pymc_statespace.utils.solve_discrete_lyapunov import doubling_step
 import numpy as np
 import aesara.tensor as at
+import aesara
+
 from typing import Tuple
 
 
@@ -8,22 +11,29 @@ class BayesianARMA(PyMCStateSpace):
 
     def __init__(self,
                  data,
-                 order: Tuple[int, int]):
+                 order: Tuple[int, int],
+                 stationary_initialization: bool = True,
+                 doubling_tol=1e-8,
+                 doubling_max_iter=100):
 
         # Model order
         self.p, self.q = order
 
-        k_states = max(self.p, self.q+1)
+        self.stationary_initialization = stationary_initialization
+        self.doubling_tol = doubling_tol
+        self.doubling_max_iter = doubling_max_iter
+
+        k_states = max(self.p, self.q + 1)
         k_posdef = 1
 
         super().__init__(data, k_states, k_posdef)
 
         # Initialize the matrices
-        self.ssm['design'] = np.r_[[1.0], np.zeros(k_states-1)][None]
+        self.ssm['design'] = np.r_[[1.0], np.zeros(k_states - 1)][None]
 
         self.ssm['transition'] = np.eye(k_states, k=1)
 
-        self.ssm['selection'] = np.r_[[[1.0]], np.zeros(k_states-1)[:, None]]
+        self.ssm['selection'] = np.r_[[[1.0]], np.zeros(k_states - 1)[:, None]]
 
         self.ssm['initial_state'] = np.zeros(k_states)[:, None]
 
@@ -32,7 +42,15 @@ class BayesianARMA(PyMCStateSpace):
         # Cache some indices
         self._state_cov_idx = ('state_cov',) + np.diag_indices(k_posdef)
         self._ar_param_idx = ('transition',) + (np.arange(self.p, dtype=int), np.zeros(self.p, dtype=int))
-        self._ma_param_idx = ('selection',) + (np.arange(1, self.q+1, dtype=int), np.zeros(self.q, dtype=int))
+        self._ma_param_idx = ('selection',) + (np.arange(1, self.q + 1, dtype=int), np.zeros(self.q, dtype=int))
+
+    @property
+    def param_names(self):
+        names = ['x0', 'P0', 'sigma_state', 'rho', 'theta']
+        if self.stationary_initialization:
+            names.remove('P0')
+
+        return names
 
     def update(self, theta: at.TensorVariable) -> None:
         """
@@ -52,10 +70,11 @@ class BayesianARMA(PyMCStateSpace):
         cursor += self.k_states
         self.ssm['initial_state', :, 0] = theta[param_slice]
 
-        # initial covariance
-        param_slice = slice(cursor, cursor + self.k_states ** 2)
-        cursor += self.k_states ** 2
-        self.ssm['initial_state_cov', :, :] = theta[param_slice].reshape((self.k_states, self.k_states))
+        if not self.stationary_initialization:
+            # initial covariance
+            param_slice = slice(cursor, cursor + self.k_states ** 2)
+            cursor += self.k_states ** 2
+            self.ssm['initial_state_cov', :, :] = theta[param_slice].reshape((self.k_states, self.k_states))
 
         # State covariance
         param_slice = slice(cursor, cursor + 1)
@@ -72,4 +91,17 @@ class BayesianARMA(PyMCStateSpace):
         cursor += self.q
         self.ssm[self._ma_param_idx] = theta[param_slice]
 
+        if self.stationary_initialization:
+            # Solve for matrix quadratic for P0
+            T = self.ssm['transition']
+            R = self.ssm['selection']
+            Q = self.ssm['state_cov']
 
+            doubling_result, doubling_updates = aesara.scan(doubling_step,
+                                                            outputs_info=[T, at.linalg.matrix_dot(R, Q, R.T)],
+                                                            non_sequences=[self.doubling_tol],
+                                                            n_steps=self.doubling_max_iter,
+                                                            name='doubling_algo')
+
+            _, P0 = doubling_result
+            self.ssm['initial_state_cov', :, :] = P0[-1]

pymc_statespace/utils/simulation.py
@@ -6,8 +6,10 @@ import numpy as np
 @njit
 def numba_mvn_draws(mu, cov):
     samples = np.random.randn(*mu.shape)
-    L = np.linalg.cholesky(cov)
+    k = cov.shape[0]
+    jitter = np.eye(k) * np.random.uniform(1e-12, 1e-8)
 
+    L = np.linalg.cholesky(cov + jitter)
     return mu + L @ samples
 
 
@@ -24,21 +26,29 @@ def conditional_simulation(mus, covs, n, k, n_simulations=100):
 @njit
 def simulate_statespace(T, Z, R, H, Q, n_steps):
     n_obs, n_states = Z.shape
-    state_noise = np.random.randn(n_steps, n_states)
-    obs_noise = np.random.randn(n_steps, n_obs)
+    k_posdef = R.shape[1]
+    k_obs_noise = H.shape[0] * (1 - int(np.all(H == 0)))
 
+    state_noise = np.random.randn(n_steps, k_posdef)
     state_chol = np.linalg.cholesky(Q)
-    obs_chol = np.linalg.cholesky(H)
-
     state_innovations = state_noise @ state_chol
-    obs_innovations = obs_noise @ obs_chol
+
+    if k_obs_noise != 0:
+        obs_noise = np.random.randn(n_steps, k_obs_noise)
+        obs_chol = np.linalg.cholesky(H)
+        obs_innovations = obs_noise @ obs_chol
 
     simulated_states = np.zeros((n_steps, n_states))
     simulated_obs = np.zeros((n_steps, n_obs))
 
-    for t in range(1, n_steps):
-        simulated_states[t] = T @ simulated_states[t - 1] + R @ state_innovations[t]
-        simulated_obs[t] = Z @ simulated_states[t - 1] + obs_innovations[t]
+    if k_obs_noise != 0:
+        for t in range(1, n_steps):
+            simulated_states[t] = T @ simulated_states[t - 1] + R @ state_innovations[t]
+            simulated_obs[t] = Z @ simulated_states[t - 1] + obs_innovations[t]
+    else:
+        for t in range(1, n_steps):
+            simulated_states[t] = T @ simulated_states[t - 1] + R @ state_innovations[t]
+            simulated_obs[t] = Z @ simulated_states[t - 1]
 
     return simulated_states, simulated_obs
 