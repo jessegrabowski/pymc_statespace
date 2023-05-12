48eb5f95a6130fecc78062d202fd03dd1960f487
jessegrabowski <jessegrabowski@gmail.com>
2023-05-12 16:33:53 +0200

    Remove unused code in pytensor_scipy.py

--- pymc_statespace/utils/pytensor_scipy.py
+++ pymc_statespace/utils/pytensor_scipy.py
@@ -5,67 +5,7 @@ import pytensor.tensor as at
 import scipy
 from pytensor.tensor import TensorVariable, as_tensor_variable
 from pytensor.tensor.nlinalg import matrix_dot
-
-
-class SolveDiscreteLyapunov(at.Op):
-    __props__ = ("method",)
-
-    def __init__(self, method=None):
-        self.method = method
-
-    def make_node(self, A, B):
-        A = as_tensor_variable(A)
-        B = as_tensor_variable(B)
-
-        out_dtype = pytensor.scalar.upcast(A.dtype, B.dtype)
-        X = pytensor.tensor.matrix(dtype=out_dtype)
-
-        return pytensor.graph.basic.Apply(self, [A, B], [X])
-
-    def perform(self, node, inputs, output_storage):
-        (A, Q) = inputs
-        X = output_storage[0]
-
-        X[0] = scipy.linalg.solve_discrete_lyapunov(A, Q, method=self.method)
-
-    def infer_shape(self, fgraph, node, shapes):
-        return [shapes[0]]
-
-    def grad(self, inputs, output_grads):
-        # Gradient computations come from Kao and Hennequin (2020), https://arxiv.org/pdf/2011.11430.pdf
-        A, Q = inputs
-        (dX,) = output_grads
-
-        X = self(A, Q)
-
-        # Eq 41, note that it is not written as a proper Lyapunov equation
-        S = self(A.conj().T, dX)
-
-        A_bar = matrix_dot(S, A, X.conj().T) + matrix_dot(S.conj().T, A, X)
-        Q_bar = S
-        return [A_bar, Q_bar]
-
-
-def solve_discrete_lyapunov(A, Q, method: Optional[str] = None) -> TensorVariable:
-    """
-    Solve the discrete Lyapunov equation :math:`AXA^H - X + Q = 0`.
-    Parameters
-    ----------
-    A: ArrayLike
-        Square matrix of shape N x N; must have the same shape as Q
-    Q: ArrayLike
-        Square matrix of shape N x N; must have the same shape as A
-    method: Optional, string
-        Solver method passed to scipy.linalg.solve_discrete_lyapunov, either "bilinear", "direct", or None. "direct"
-        scales poorly with size. If None, uses "direct" if N < 10, else "bilinear".
-
-    Returns
-    -------
-    X: at.matrix
-        Square matrix of shape N x N, representing the solution to the Lyapunov equation
-    """
-
-    return SolveDiscreteLyapunov(method)(A, Q)
+from pytensor.tensor.slinalg import solve_discrete_lyapunov
 
 
 class SolveDiscreteARE(at.Op):
@@ -141,26 +81,3 @@ def solve_discrete_are(A, B, Q, R) -> TensorVariable:
     """
 
     return SolveDiscreteARE()(A, B, Q, R)
-
-
-def allocate_block(A, out, r_start, c_start, r_stride, c_stride):
-    row_slice = slice(r_start, r_start + r_stride)
-    col_slice = slice(c_start, c_start + c_stride)
-
-    next_r = r_start + r_stride
-    next_c = c_start + c_stride
-
-    return at.set_subtensor(out[row_slice, col_slice], A), next_r, next_c
-
-
-def block_diag(arr: at.tensor3):
-    n, rows, cols = arr.shape
-    out = at.zeros((n * rows, n * cols))
-
-    result, _ = pytensor.scan(
-        allocate_block,
-        sequences=[arr],
-        outputs_info=[out, at.zeros(1, dtype="int64"), at.zeros(1, dtype="int64")],
-        non_sequences=[rows.astype("int64"), cols.astype("int64")],
-    )
-    return result[0][-1]
